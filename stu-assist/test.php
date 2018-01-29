d993e9ca9fa17491deb793020116c23f7d889489
杨磊 <452857163@qq.com>
2018-01-29 23:14:39 +0800

    PHP实现中文字符串反转(面试题)

--- stu-assist/test.php
+++ stu-assist/test.php
@@ -1,3 +1,15 @@
 <?php
-var_dump(1);
+function getRev($str,$encoding='utf-8')
+{
+    $result = '';
+    $len = mb_strlen($str);
+    for($i=$len-1; $i>=0; $i--){
+        $result .= mb_substr($str,$i,1,$encoding);
+    }
+    return $result;
+}
+
+$str = '中文字符串';
+$revStr = getRev($str);
+var_dump($revStr);
 die;
